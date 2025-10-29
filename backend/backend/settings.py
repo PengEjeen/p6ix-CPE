f808ea05a9e8571388fbf91429f32a71e252def8
PengEjeen <chony5093@gmail.com>
2025-10-30 08:21:48 +0900

    user update 추가

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -153,7 +153,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),  # 토큰 유효기간
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),  # 토큰 유효기간
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
